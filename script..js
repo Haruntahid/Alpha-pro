5d75231d8cb0cdcee4c6efa8441746e610661e89
haruntahid <tahidtaha997@gmail.com>
2024-02-15 14:46:40 +0600

    all functionalities added

--- script..js
+++ script..js
@@ -20,11 +20,74 @@ function setKeyColor(elementId) {
   element.classList.add("bg-amber-500");
 }
 
+function removeKeyColor(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.remove("bg-amber-500");
+}
+
+function getInnerTextById(elementId) {
+  const getElement = document.getElementById(elementId);
+  const elementText = getElement.innerText;
+  const value = parseInt(elementText);
+  return value;
+}
+
+function updateValue(elementId, value) {
+  const getElement = document.getElementById(elementId);
+  getElement.innerText = value;
+}
+
+function getPressKey(event) {
+  const pressKeyValue = event.key;
+  const getGivenValue = document.getElementById("generate_alpha");
+  const givenValueText = getGivenValue.innerText;
+  const givenValue = givenValueText.toLowerCase();
+
+  if (pressKeyValue === "Escape") {
+    gameOver();
+  }
+
+  if (pressKeyValue === givenValue) {
+    play();
+    removeKeyColor(givenValue);
+    const score = getInnerTextById("score");
+    const updateScore = score + 1;
+    updateValue("score", updateScore);
+  } else {
+    const life = getInnerTextById("life");
+    const updateLife = life - 1;
+    updateValue("life", updateLife);
+
+    if (updateLife === 0) {
+      gameOver();
+    }
+  }
+}
+
+document.addEventListener("keyup", getPressKey);
+
 function play() {
   hideSection("enter_game");
   displaySection("playground");
+  hideSection("score_card");
+  updateValue("life", 5);
+  // updateValue("score", 0);
+  continueGame();
+}
+
+function continueGame() {
   let alpha = randomAlpha();
   let setAlpha = document.getElementById("generate_alpha");
   setAlpha.innerText = alpha;
   setKeyColor(alpha);
+}
+
+function gameOver() {
+  hideSection("playground");
+  displaySection("score_card");
+  const score = getInnerTextById("score");
+  updateValue("final_score", score);
+  const alphabet = document.getElementById("generate_alpha");
+  const alphabetValue = alphabet.innerText;
+  removeKeyColor(alphabetValue);
 }
